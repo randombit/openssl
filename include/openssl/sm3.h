27f8c51f7c6bdaf8426671b3b4098c6dbd57e5cf
Jack Lloyd <jack.lloyd@ribose.com>
2017-10-25 13:38:06 -0400

    Add docs

--- include/openssl/sm3.h
+++ include/openssl/sm3.h
@@ -37,6 +37,7 @@ typedef struct SM3state_st {
 int SM3_Init(SM3_CTX *c);
 int SM3_Update(SM3_CTX *c, const void *data, size_t len);
 int SM3_Final(unsigned char *md, SM3_CTX *c);
+void SM3_Transform(SM3_CTX *c, const unsigned char *data);
 unsigned char *SM3(const unsigned char *d, size_t n, unsigned char *md);
 
 # ifdef  __cplusplus
